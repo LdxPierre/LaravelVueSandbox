513880d3648e5555e5ee3690a80bba52604bab0f
LdxPierre <pierre.ledoux@outlook.fr>
2025-01-04 17:50:38 +0100

    Tailwind, Reset & first page

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="w-full px-3 py-2 flex gap-3">
+    <RouterLink to="home">Home</RouterLink>
+    <RouterLink to="login">Login</RouterLink>
+  </header>
+</template>
---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <form @submit.prevent>
+    <input type="text" name="email" id="email" />
+    <input type="password" name="password" id="password" />
+    <input type="submit" value="Connexion" />
+  </form>
+</template>
